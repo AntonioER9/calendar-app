a92a3bb88a1a41d59256c9109856aa48328783c7
Antonio Espinoza <aespinozareyes9@gmail.com>
2024-01-30 16:07:43 -0300

    add config of react router dom for management of routes

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,21 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { LoginPage } from '../auth';
+import { CalendarPage } from '../calendar';
+
+
+//Componente en el cual llevaremos el manejo de las rutas
+export const AppRouter = () => {
+
+  const authStatus = 'not-authenticated'; //authenticated
+
+  return (
+    <Routes>
+      {
+        (authStatus === 'not-authenticated')
+          ? <Route path='/auth/*' element={<LoginPage />}/>
+          : <Route path='/*' element={<CalendarPage />}/>
+      }
+      <Route path='/*' element={<Navigate to='/auth/login' />}/>
+    </Routes>
+  )
+}
