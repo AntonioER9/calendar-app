915307f4a65cb6f9f0793250a681fe786b5ca778
Antonio Espinoza <aespinozareyes9@gmail.com>
2024-01-31 14:28:02 -0300

    add the calendar slice

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,34 +1,19 @@
 import { Calendar } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
-import { addHours } from 'date-fns';
 import { CalendarEvent, CalendarModal, Navbar } from "../";
 
 import { localizer, getMessagesES } from '../../helpers';
 import { useState } from 'react';
-import { useUiStore } from '../../hooks';
+import { useUiStore, useCalendarStore } from '../../hooks';
 
-
-
-
-const events = [{
-  title: 'Cumpleaños',
-  notes: 'Hay que comprar un pastel',
-  start: new Date(),
-  end: addHours( new Date(), 2),
-  bgColor: '#fafafa',
-  user: {
-    _id: '123',
-    name: 'Antonio'
-  }
-
-}]
 
 export const CalendarPage = () => {
 
-  const {openDateModal} = useUiStore();
+  const { openDateModal } = useUiStore();
+  const { events } = useCalendarStore();
 
-  const [lastView, setlastView] = useState(localStorage.getItem('lastView') || 'week');
+  const [ lastView, setlastView ] = useState(localStorage.getItem('lastView') || 'week');
 
   const eventStyleGetter = (event, start, end, isSelected) => {
   
--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { uiSlice } from './';
+import { uiSlice, calendarSlice } from './';
 
 //Almacen de información de toda nuestra app
 export const store = configureStore({
     reducer: {
+        calendar: calendarSlice.reducer,
         ui: uiSlice.reducer
     }
 })
