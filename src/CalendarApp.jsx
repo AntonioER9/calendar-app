dcc82f2228bdb3b767ea0bfd06eaa70db5967a9e
Antonio Espinoza <aespinozareyes9@gmail.com>
2024-02-06 11:16:32 -0300

    another way to use routes in production

--- src/CalendarApp.jsx
+++ src/CalendarApp.jsx
@@ -1,6 +1,6 @@
 import { Provider } from 'react-redux';
 import { AppRouter } from './router/';
-import { BrowserRouter} from 'react-router-dom';
+import { BrowserRouter, HashRouter} from 'react-router-dom';
 import { store } from './store';
 
 //Provider store Necesario para dejar nuestro store en la parte más alta de la aplicación
@@ -9,7 +9,9 @@ export const CalendarApp = () => {
   return (
     <Provider store={store}>
       <BrowserRouter> 
+      {/* <HashRouter> otro sistema de manejo de rutas para despliegue */}
         <AppRouter />
+      {/* </HashRouter> */}
       </BrowserRouter>
     </Provider>
   )
